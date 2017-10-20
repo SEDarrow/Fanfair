50a69acbd46b7062545c91f368176399cd3a33e6
SEDarrow <sdarrow0519@gmail.com>
2017-10-20 09:49:12 -0500

    Updated create tables code for database

--- database/tables.sql
+++ database/tables.sql
@@ -1,29 +1,42 @@
 CREATE TABLE `user` (
   `username` varchar(128),
   `password` varchar(128),
-  `score` int(11),
+  `score` int,
   PRIMARY KEY (`username`)
 );
 
 CREATE TABLE `playlist` (
   `pid` varchar(16),
+  `add` boolean,
   `username` varchar(128),
-  PRIMARY KEY (`pid`,`username`),
+  PRIMARY KEY (`pid`),
   FOREIGN KEY (`username`) REFERENCES `user` (`username`)
-);
-
-CREATE TABLE `songInfo` (
-  `url` varchar(128),
-  `title` varchar(128),
-  PRIMARY KEY (`url`)
 );
 
 CREATE TABLE `song` (
   `url` varchar(128),
+  `sid` varchar(16),
+  `title` varchar(256),
+  PRIMARY KEY (`sid`)
+);
+
+CREATE TABLE `playist_contains_song` (
   `pid` varchar(16),
-  `upvotes` int(11),
-  `downvotes` int(11),
-  PRIMARY KEY (`url, pid`)
-  FOREIGN KEY (`pid`) REFERENCES `playlist` (`pid`)
-  FOREIGN KEY (`url`) REFERENCES `songInfo` (`url`)
+  `sid` varchar(16),
+  `upvote` int,
+  `downvote` int,
+  `encore` int,
+  PRIMARY KEY (`pid`,`sid`),
+  FOREIGN KEY (`pid`) REFERENCES `playlist` (`pid`),
+  FOREIGN KEY (`sid`) REFERENCES `song` (`sid`)
+);
+
+CREATE TABLE `vote` (
+  `username` varchar(128),
+  `pid` varchar(16),
+  `sid` varchar(16),
+  PRIMARY KEY (`pid`,`username`, `sid`),
+  FOREIGN KEY (`username`) REFERENCES `user` (`username`),
+  FOREIGN KEY (`pid`) REFERENCES `playlist` (`pid`),
+  FOREIGN KEY (`sid`) REFERENCES `song` (`sid`)
 );
