0521a6eae3ff4f1df7ee42c03c1182c43931d63f
Jimmy Nguyen <NguyenJim6@gmail.com>
2017-11-09 16:10:02 -0600

    add create_user();

--- database/database_functions.php
+++ database/database_functions.php
@@ -2,6 +2,33 @@
 // database_functions.php
 // Description: Functions that pertain to the mysql database
 
+    function create_user($uname, $pword)
+    {
+        /*
+         * Description: Add user to database
+         * Parameters:
+         * |   Param    |   Type    |   Description     |
+         * Returns: None
+         */
+        $conn = conn_start();
+
+        // sanitize input
+        $uname = sanitize($conn, $uname);
+        $pword = sanitize($conn, $pword); 
+
+        // salt pword
+        $salt1 = "j*H2!";
+        $salt2 = "9@l#o";
+        $pword_hashed = hash('ripemd128', "$salt1$pword$salt2");
+
+        // default score to 0 and admin flag to false
+        $query = "INSERT user(username, password, score, admin) VALUES('$uname', '$pword_hashed', 0, false)";
+        /* echo "$query"; // for debugging */
+        executeQuery($conn, $query); 
+
+        $conn->close();
+    }
+    
     function executeQuery($conn, $query)
     {
         /*
@@ -20,6 +47,7 @@
 
         // if no result, MySQL Error.  Close connection
         if (!$result) {
+            echo "$conn->error";
             $conn->close();
             return 1; 
         }
@@ -41,7 +69,7 @@
         return $returnValue;
     }
  
-    function conn_start();
+    function conn_start()
     {
         /*
          * Description: Connect to the Database
--- database/database_test.php
+++ database/database_test.php
@@ -2,8 +2,12 @@
 
 require_once('database_functions.php');
 
+    /*
     executeQuery("INSERT user(username, password, score, admin)
                   VALUES('Logan', 'apples', 0, false)");
     executeQuery("INSERT user(username, password, score, admin)
-                  VALUES('Drake', 'dick', 0, false)");
+                  VALUES('Drake', 'banana', 0, false)");
+    */
+    
+    //create_user("admin", "admin");
 ?>
