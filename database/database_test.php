19d18843595e8238db274313347aae90b5dd347f
Jimmy Nguyen <NguyenJim6@gmail.com>
2017-11-09 21:25:33 -0600

    Modified tables.sql: pid, sid are auto-increment

--- database/database_test.php
+++ database/database_test.php
@@ -9,9 +9,28 @@ require_once('database_functions.php');
                   VALUES('Drake', 'banana', 0, false)");
     */
     
+    /*
     create_user("admin", "admin");
     create_user("Jimmy", "test");
     create_user("Sara", "test");
     create_user("Landon", "banana");
     promote_to_admin("admin");
+     */
+    
+    /*
+    $conn = conn_start();
+
+    // string sanitation
+    $uname = sanitize($conn, 'admin');
+    $pword = sanitize($conn, 'admin');
+
+    // password salts
+    $salt1 = "j*H2!";
+    $salt2 = "9@l#o";
+    $pword_hashed = hash('ripemd128', "$salt1$pword$salt2");
+
+    $query = "SELECT * FROM user WHERE username='$uname' AND password='$pword_hashed'"; 
+    results = executeQuery($conn, $query); 
+    $conn->close(); 
+     */
 ?>
--- database/tables.sql
+++ database/tables.sql
@@ -7,23 +7,24 @@ CREATE TABLE `user` (
 );
 
 CREATE TABLE `playlist` (
-  `pid` varchar(16),
+  `pid` int(16) AUTO_INCREMENT,
   `add` boolean,
   `username` varchar(128),
+  `playlist_name` varchar(128),
   PRIMARY KEY (`pid`),
   FOREIGN KEY (`username`) REFERENCES `user` (`username`)
 );
 
 CREATE TABLE `song` (
   `url` varchar(128),
-  `sid` varchar(16),
+  `sid` int(16) AUTO_INCREMENT,
   `title` varchar(256),
   PRIMARY KEY (`sid`)
 );
 
-CREATE TABLE `playist_contains_song` (
-  `pid` varchar(16),
-  `sid` varchar(16),
+CREATE TABLE `playlist_contains_song` (
+  `pid` int(16),
+  `sid` int(16),
   `upvote` int,
   `downvote` int,
   `encore` int,
@@ -34,8 +35,8 @@ CREATE TABLE `playist_contains_song` (
 
 CREATE TABLE `vote` (
   `username` varchar(128),
-  `pid` varchar(16),
-  `sid` varchar(16),
+  `pid` int(16),
+  `sid` int(16),
   PRIMARY KEY (`pid`,`username`, `sid`),
   FOREIGN KEY (`username`) REFERENCES `user` (`username`),
   FOREIGN KEY (`pid`) REFERENCES `playlist` (`pid`),
