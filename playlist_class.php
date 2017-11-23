1210b0580dad2f44d0456b8004b88dc314ad81eb
Jimmy Nguyen <NguyenJim6@gmail.com>
2017-11-22 18:06:56 -0600

    Tested/Debugged playlist_class

--- playlist_class.php
+++ playlist_class.php
@@ -13,18 +13,16 @@ class Playlist {
     private $encore_score;
     private $song_list;
 
-    function __construct($owner, $pid)
+    function __construct($pid)
     {
         $conn = conn_start();
-
-        $this->owner = sanitize($conn, $owner);
         $this->pid = sanitize($conn, $pid);
-
-        $result = executeQuery($conn, "SELECT * FROM playlist WHERE username='$this->owner' AND pid='$this->pid'");
-
+        $result = executeQuery($conn, "SELECT * FROM playlist WHERE pid='$this->pid'")[0];
         $conn->close();
-        $this->pid = $result[0]["pid"];
-        $this->name = $result[0]["playlist_name"];
+
+        $this->owner = $result["username"];
+        $this->name = $result["playlist_name"];
+        $this->update_song_list();      // populate the song list
     }
 
     function add_song($url, $uploader, $title)
@@ -32,15 +30,15 @@ class Playlist {
         $song = new Song($url, $uploader, $this->pid, $title);
         $sid = $song->get_sid();
         $conn = conn_start();
-        executeQuery($conn, "INSERT INTO playlist_contains_song (pid, sid, uploader) VALUES ('$this->pid', '$sid', '$uploader')");
+        executeQuery($conn, "INSERT INTO playlist_contains_song (pid, sid, uploader, upvote, downvote, encore) VALUES ('$this->pid', '$sid', '$uploader', 0, 0, 0)");
         $conn->close();
     }
 
     // song parameter is sid
-    function remove_song($song)
+    function remove_song($sid)
     {
         $conn = conn_start();
-        executeQuery($conn, "DELETE FROM playlist_contains_song WHERE sid='$song' AND pid='$this->pid'");
+        executeQuery($conn, "DELETE FROM playlist_contains_song WHERE sid='$sid' AND pid='$this->pid'");
         $conn->close();
     }
 
@@ -52,7 +50,7 @@ class Playlist {
 
     function get_owner_username()
     {
-        return $this->owner_username;
+        return $this->owner;
     }
 
     function get_current_song()
@@ -64,7 +62,7 @@ class Playlist {
     function update_current_song()
     {
         $conn = conn_start();
-        $result = executeQuery($conn, "SELECT url, title, uploader FROM playlist_contains_song P, song S WHERE upvotes-downvotes = (SELECT MAX(upvotes - downvotes) FROM playlist_contains_song WHERE pid = $this->pid) AND pid = $this->pid AND P.sid = S.sid");
+        $result = executeQuery($conn, "SELECT url, title, uploader FROM playlist_contains_song P, song S WHERE upvote-downvote = (SELECT MAX(upvote - downvote) FROM playlist_contains_song WHERE pid = $this->pid) AND pid = $this->pid AND P.sid = S.sid");
         $conn->close();
 
         // get the last element in the array (or the song that has been in the database the longest)
@@ -77,8 +75,9 @@ class Playlist {
 
     function update_song_list()
     {
+        $this->song_list = [];
         $conn = conn_start();
-        $result = executeQuery($conn, "SELECT * FROM playlist_contains_song P, song S WHERE pid = $this->pid AND P.sid = S.sid ORDER BY (upvotes - downvotes) DESC");
+        $result = executeQuery($conn, "SELECT * FROM playlist_contains_song P, song S WHERE pid = $this->pid AND P.sid = S.sid ORDER BY (upvote - downvote) DESC");
         $conn->close();
 
         $songs = [];
--- playlist_class_test.php
+++ playlist_class_test.php
@@ -5,10 +5,94 @@
 
     // init object
     $my_playlist = new Playlist(1);
-    $songs = $my_playlist->get_song_list();
-    foreach ($songs as $song)
+
+    // 1) get_playlist_name()
+    echo "Playlist Name:  ";
+    print_r($my_playlist->get_playlist_name());
+    echo "<br><br>";
+
+    // 2) get_owner_username()
+    echo "Owner Name:  ";
+    print_r($my_playlist->get_owner_username());
+    echo "<br><br>";
+
+    // 3) get_song_list()
+    echo "Song List:<br>";
+    $song_list = $my_playlist->get_song_list();
+    foreach($song_list as $song)
     {
-        print("Title: $song->get_title()    Url:  $song->get_url()<br>"); 
+        echo ">>>";
+        print_r($song);
+        echo "<br>";
     }
+    $cnt = count($song_list);
+    echo "Song count: $cnt";
+    echo "<br>";
+
+    // 4) get_current_song()
+    echo "Current Song:  ";
+    print_r($my_playlist->get_current_song());
+    echo "<br><br>";
+
+    /*
+    // 5) remove_current_song()
+    echo "Removing current song...<br><br>";
+    $my_playlist->remove_current_song();
+
+    // 5.1) get_current_song()  Should be next song
+    echo "Current Song:  ";
+    print_r($my_playlist->get_current_song());
+    echo "<br><br>";
+     */
+
+    // 6) remove_song($sid) Song should be missing
+    $sid = 4;
+    echo "Removing Specifig Song: Pid = $sid";
+    $my_playlist->remove_song($sid);
+    echo "<br>";
+
+    // 6.1) get_song_list()
+    echo "Song List:<br>";
+    $song_list = $my_playlist->get_song_list();
+    foreach($song_list as $song)
+    {
+        echo ">>>";
+        print_r($song);
+        echo "<br>";
+    }
+    $cnt = count($song_list);
+    echo "Song count: $cnt";
+    echo "<br>";
+
+    // 7) add_song($url, $uploader, $title)
+    $url = "rkStmZMfBEY";
+    $uploader = "Jimmy";
+    $title = "I Got Top At Bikini Bottom";
+    echo "Adding song... url: $url    uploader: $uploader    title: $title<br><br>";
+    $my_playlist->add_song($url, $uploader, $title);
+
+    // 7.1) check that the song was added
+    echo "Song List:<br>";
+    $song_list = $my_playlist->get_song_list();
+    foreach($song_list as $song)
+    {
+        echo ">>>";
+        print_r($song);
+        echo "<br>";
+    }
+    $cnt = count($song_list);
+    echo "Song count: $cnt";
+    echo "<br><br>";
+
+    // 8) vote_encore() & get_encore_score()
+    echo "Testing Encore... Current Song: ";
+    print_r($my_playlist->get_current_song());
+    echo ".... encore count: "; print_r($my_playlist->get_encore_score());
+    echo "<br>";
+    echo "After Encore function:  ";
+    $my_playlist->vote_encore();
+    print_r($my_playlist->get_current_song());
+    echo ".... encore count: "; print_r($my_playlist->get_encore_score());
+    echo "<br><br>";
 
 ?>
