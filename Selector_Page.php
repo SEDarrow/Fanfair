225fe912cb2fdfbdbc799eebc5a465cbd83dd5f5
SEDarrow <sdarrow0519@gmail.com>
2017-11-27 22:53:18 -0600

    Formatted playlist page

--- Selector_Page.php
+++ Selector_Page.php
@@ -1,25 +1,8 @@
 <!DOCTYPE html>
 <html>
-<style> 
-td, th {
-     border: 1px solid;
-     text-align: center;
-     padding: 0.5em;
-  }  
-  input[type='radio'] { transform: scale(4); }
-	input[type='submit'] { transform: scale(4); }
-
-</style>
-
     <head>
         <meta charset="UTF-8">
-        <title>Fanfair - Log In
-		</title>
-        <style>
-            input {
-                margin-bottom: 0.5em;
-            }
-        </style>
+        <title>Fanfair - Playlists</title>
 		<link href="https://fonts.googleapis.com/css?family=Oleo+Script" rel="stylesheet">
 		<link rel="stylesheet" href="style/common.css">
     </head>
@@ -54,8 +37,8 @@ ini_set('display_errors', 1);
 	}
 ?>
 <?php 
-	echo "<font size='10' face='Arial'>";
-	echo "Current Artists";
+	echo "<center>";
+	echo "<h1 style='border-bottom-style:solid;width:35%;font-size:55px;'>Current Playlists</h1>";
 	require_once('database/database_functions.php');
 	$conn = conn_start();
 	$query = "SELECT * FROM playlist";
@@ -65,30 +48,22 @@ ini_set('display_errors', 1);
 		
 			
 				
-	$rows = $result->num_rows;?>
-	
+	$rows = $result->num_rows;
+?>
+	</center>
 	<form method = 'POST' action = ''>
 	
-	<?php
-	for($j =0; $j < $rows; $j++){ //This loop scans the database for current playlists.
-			
-			$result->data_seek($j);
-			$row = $result->fetch_array(MYSQLI_ASSOC);
-			
-			echo "<font size='18' face='Arial'>";
-			echo $row['username'];
-			
-			$name = ((string)$row['pid']); //Grabbing the PID from the database
-			
-	?>
-			<input type = "Radio" name = 'playlist' value=<?php echo($name) ?>> 
-			<br>
-	<?php
-		}
-	?>
+<?php
+	for($j =0; $j < $rows; $j++){ //This loop scans the database for current playlists.	
+		$result->data_seek($j);
+		$row = $result->fetch_array(MYSQLI_ASSOC);
+		$name = ((string)$row['pid']); //Grabbing the PID from the database
+		echo "<h2 style='margin-left:30%'> <input type='radio' name='playlist' value=$name>" . $row['username'] . "</h2>";
+	}
+?>
+	<input id='button' type='submit' name = 'Submit Button' value ='View Playlist' style='position:fixed;bottom:20px;left:45%'>
 	
 	<br>
-	<input type="submit" name = "Submit Button" value = 'Go!' >
 	
 	<?php for($t =0; $t < 1; $t++){
 		if(isset($_POST['playlist'])){
@@ -104,6 +79,7 @@ ini_set('display_errors', 1);
 	?>
 	
 	</form>
+	</div>
 	<?php 
 	
 	
